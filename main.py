da7c0e06e5caabb34b9a654ba2472604ecb87b0a
ghostrowdy <markusachrainer@gmail.com>
2025-10-22 19:47:33 +0200

    added display word amount function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
-    get_book_text()
+    display_word_amount()
 
 def get_book_text():
     with open('books/frankenstein.txt') as f:
         print(f.read())
+
+def display_word_amount():
+    with open('books/frankenstein.txt') as f:
+        word_list = f.read().split()
+        print(f'Found {len(word_list)} total words')
 
 main()
